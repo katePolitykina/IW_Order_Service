0e426804d96ab8f46f58e7a869144ad7c3849e10
ekaterinapolitykina <eka.politykina@gmail.com>
2025-07-31 10:50:08 -0400

    DAO Layer

---
+++ src/main/java/org/example/iw_order_service/repository/OrderRepository.java
@@ -0,0 +1,20 @@
+package org.example.iw_order_service.repository;
+
+import org.example.iw_order_service.entity.Order;
+import org.example.iw_order_service.entity.enums.OrderStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    Stream<Order> findAllByIdIn(List<Long> ids);
+    Stream<Order> findAllByStatusIn(List<OrderStatus> statuses);
+    default Optional<Order> update(Order input) {
+        return Optional.of(save(input));
+    }
+
+}
